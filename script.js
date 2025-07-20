f524304602e5d7a2f33a201ec65fa1b8fb6f82dd
daaniii13 <danmunozmtz@gmail.com>
2025-07-20 02:35:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,6 @@ window.addEventListener('scroll', () => {
   const docHeight = document.documentElement.scrollHeight - window.innerHeight;
   const scrollPercent = (scrollTop / docHeight) * 100;
   const progressBar = document.getElementById('progress-bar');
-  if (progressBar) {
-    progressBar.style.width = scrollPercent + '%';
-  }
+  progressBar.style.height = scrollPercent + '%';
 });
+
